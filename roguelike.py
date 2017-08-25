bab46c70162897b8044e72e172983c55bbedcb23
DrMayx <mj6367@gmail.com>
2017-08-25 11:34:53 +0200

    add menu graphics files and the main game loop

--- roguelike.py
+++ roguelike.py
@@ -2,11 +2,15 @@ from clear import resize_and_clear as clear
 from movement import getch
 
 def main():
-	clear()
-	print(getch())
-"""
-while True:
-"""
+	while True:
+		clear()
+		user_input = getch()
+		if user_input == "":				#if users clicks escape button the program terminates
+			break
+		elif user_input == 'p':
+			break
+
+
 if __name__ == "__main__":
 	main()
 	
