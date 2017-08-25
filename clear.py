b81c525cbfb7ea119900fda22b6c6ec318c6e8ca
DrMayx <mj6367@gmail.com>
2017-08-25 10:17:40 +0200

    add input read, improve clear optimalisation, implement mentioned changes in game core.

--- clear.py
+++ clear.py
@@ -3,10 +3,5 @@ import os
 
 def resize_and_clear():
 	stdout.write("\x1b[8;{rows};{cols}t".format(rows=50, cols=150))
+	print("\n"*5)
 	os.system('cls' if os.name == 'nt' else 'clear')
-
-
-def main():
-	resize_and_clear()
-
-main()
--- roguelike.py
+++ roguelike.py
@@ -1,3 +1,12 @@
-from main import main
+from clear import resize_and_clear as clear
+from movement import getch
 
-main()
+def main():
+	clear()
+	print(getch())
+"""
+while True:
+"""
+if __name__ == "__main__":
+	main()
+	
